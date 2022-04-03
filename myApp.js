09d53f464c30f16831f8568c1123486cb2190c58
jeanqueres <jean.souzaqueres@gmail.com>
2022-04-03 11:16:05 -0300

    Install and Set Up Mongoose

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const mongoose = require("mongoose");
 
+console.log('uri', process.env.MONGO_URI);
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 let Person;

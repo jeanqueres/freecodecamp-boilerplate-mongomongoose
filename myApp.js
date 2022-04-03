4841bf92ecbb5cb28e65ab817160ca5b3a8a451c
jeanqueres <jean.souzaqueres@gmail.com>
2022-04-03 20:07:51 -0300

    Chain Search Query Helpers to Narrow Search Results

--- myApp.js
+++ myApp.js
@@ -116,7 +116,7 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
   Person.find({favoriteFoods: foodToSearch})
-  .sort({ 'name': 'desc' })
+  .sort({ 'name': 'asc' })
   .limit(2)
   .select('-age')
   .exec((err, data) => { 
